1e1673da7f9f5b7339b2c680e8c3afc123cbaa97
mooaudaud <mooaudaud@gmail.com>
2023-12-14 14:48:56 +0700

    Update callback.js

--- pages/callback.js
+++ pages/callback.js
@@ -2,18 +2,24 @@ import { useState, useEffect } from "react";
 import liff from "@line/liff";
 
 const Callback = (props) => {
+  const [params, setParams] = useState({})
+  const [profile, setProfile] = useState({})
   useEffect(() => {
     initPage()
   }, [])
 
   const initPage = async () => {
     await liff.init({ liffId: process.env.LIFF_ID })
-    liff.getContext()
     liff.login()
+    const profile = await liff.getProfile()
+    setProfile(profile)
   }
 
   const params = new URLSearchParams(window.location.search);
-  return <p>{JSON.stringify(params)}</p>;
+  return <div>
+    <p>{JSON.stringify(params)}</p>
+  <p>{JSON.stringify(profile)}</p>
+  </div>;
 }
 
 export default Callback
