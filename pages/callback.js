4ce8aa17fb7729cc5503be8a41fba8c0cfebed9f
mooaudaud <mooaudaud@gmail.com>
2023-12-14 14:44:26 +0700

    Update callback.js

--- pages/callback.js
+++ pages/callback.js
@@ -1,1 +1,19 @@
+import { useState, useEffect } from "react";
+import liff from "@line/liff";
 
+const Callback = (props) => {
+  useEffect(() => {
+    initPage()
+  }, [])
+
+  const initPage = async () => {
+    await liff.init({ liffId: process.env.LIFF_ID })
+    liff.getContext()
+    liff.login()
+  }
+
+  const params = new URLSearchParams(window.location.search);
+  return <p>{JSON.stringify(params)}</p>;
+}
+
+export default Callback
